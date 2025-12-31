31edee54c6a9660737e814420b6e8c3ff151515a
Uduak Joshua <uduakjoshua@gmail.com>
2025-12-31 11:59:06 +0100

    commented out all test route

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,8 @@ use App\Http\Controllers\Api\NewsController;
 // routing for the endpoint for the frontend to get aggregated news articles
 Route::get('/articles', [ArticleController::class, 'index']);
 
-// API routes for triggering news fetch from different sources for testing purposes
+/*/ API routes for triggering news fetch from different sources for testing purposes
 Route::prefix('news')->group(function () {
     Route::get('/guardian', [NewsController::class, 'fetchGuardian']);
     Route::get('/newsapi', [NewsController::class, 'fetchNewsApi']);
-});
+});*/
